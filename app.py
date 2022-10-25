70a163f05cc2901e1048313a852029fa42b3e4fa
Chayoot Kosiwanich <60844731+khunfloat@users.noreply.github.com>
2022-10-25 22:59:54 +0700

    fix cor

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 import firebase_admin
 from firebase_admin import credentials, firestore
 
@@ -7,6 +8,7 @@ firebase_admin.initialize_app(cred)
 db = firestore.client()
 
 app = Flask(__name__)
+CORS(app)
 
 from routes import index, route, home, explore
 
