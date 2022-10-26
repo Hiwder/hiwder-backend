8f603f66f76b791e7ad5f2a95357e06c2489027c
Chayoot Kosiwanich <60844731+khunfloat@users.noreply.github.com>
2022-10-26 20:23:18 +0700

    create new api [ /near-you ]

--- routes/explore.py
+++ routes/explore.py
@@ -1,5 +1,6 @@
 from app import app, db
 from flask import make_response, request
+import geopy.distance
 
 @app.route('/explore', methods=['POST'])
 def explore():
@@ -19,4 +20,29 @@ def explore():
     else:
         return make_response({"error" : "this api request 1 parameters (tag)"}, 200)
 
-    
+@app.route('/near-you', methods=['POST'])
+def near_you():
+
+    near_items = list()
+
+    items = [item.to_dict() for item in db.collection('items').stream()]
+
+    data = request.get_json()
+
+    if ("location" and "radius") in data:
+
+        for item in items:
+            dst = tuple(item["location"])
+            org = tuple(data["location"])
+            radius = float(data["radius"])
+
+            distance = geopy.distance.geodesic(org, dst).km
+
+            if distance <= radius:
+                item["distance"] = float("{0:.1f}".format(distance))
+                near_items.append(item)
+
+        return make_response({"items" : near_items}, 200)
+
+    else:
+        return make_response({"error" : "this api request 2 parameters ([location], radius)"}, 200)
